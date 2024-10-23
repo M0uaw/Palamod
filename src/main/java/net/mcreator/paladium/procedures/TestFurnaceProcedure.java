73cfee5641efcd853ff53c171e1005368e0aed67
M0uaw <scratch.thomas.catteau@gmail.com>
2024-10-24 01:09:33 +0200

    Procedure "TestFurnace" Upgraded

--- src/main/java/net/mcreator/paladium/procedures/TestFurnaceProcedure.java
+++ src/main/java/net/mcreator/paladium/procedures/TestFurnaceProcedure.java
@@ -1,7 +1,9 @@
 package net.mcreator.paladium.procedures;
 
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.ForgeHooks;
 
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.crafting.RecipeType;
@@ -14,7 +16,6 @@ import net.minecraft.tags.ItemTags;
 import net.minecraft.resources.ResourceLocation;
 
 import net.mcreator.paladium.init.PaladiumModItems;
-import net.mcreator.paladium.PaladiumMod;
 
 import java.util.function.Supplier;
 import java.util.concurrent.atomic.AtomicReference;
@@ -24,23 +25,67 @@ public class TestFurnaceProcedure {
 	public static void execute(LevelAccessor world, Entity entity, ItemStack itemstack) {
 		if (entity == null)
 			return;
-		if (world instanceof Level _level2 && _level2.getRecipeManager().getRecipeFor(RecipeType.SMELTING, new SimpleContainer((new Object() {
-			public ItemStack getItemStack(int sltid, ItemStack _isc) {
-				AtomicReference<ItemStack> _retval = new AtomicReference<>(ItemStack.EMPTY);
-				_isc.getCapability(ForgeCapabilities.ITEM_HANDLER, null).ifPresent(capability -> {
-					_retval.set(capability.getStackInSlot(sltid).copy());
-				});
-				return _retval.get();
-			}
-		}.getItemStack(1, itemstack))), _level2).isPresent()
-				&& (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(0)).getItem() : ItemStack.EMPTY)
-						.is(ItemTags.create(new ResourceLocation("minecraft:combustible")))
+		double fuel_power = 0;
+		double number_furnace_upgrade = 0;
+		if ((entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(0)).getItem() : ItemStack.EMPTY)
+				.is(ItemTags.create(new ResourceLocation("minecraft:combustible"))) && world instanceof Level _level4 && _level4.getRecipeManager().getRecipeFor(RecipeType.SMELTING, new SimpleContainer((new Object() {
+					public ItemStack getItemStack(int sltid, ItemStack _isc) {
+						AtomicReference<ItemStack> _retval = new AtomicReference<>(ItemStack.EMPTY);
+						_isc.getCapability(ForgeCapabilities.ITEM_HANDLER, null).ifPresent(capability -> {
+							_retval.set(capability.getStackInSlot(sltid).copy());
+						});
+						return _retval.get();
+					}
+				}.getItemStack(1,
+						itemstack))), _level4).isPresent()
+				&& ((entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(2)).getItem() : ItemStack.EMPTY).getItem() == Blocks.AIR.asItem()
+						|| (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(2)).getItem() : ItemStack.EMPTY)
+								.getItem() == (world instanceof Level _lvlSmeltResult
+										? _lvlSmeltResult.getRecipeManager()
+												.getRecipeFor(RecipeType.SMELTING,
+														new SimpleContainer(
+																(entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY)),
+														_lvlSmeltResult)
+												.map(recipe -> recipe.getResultItem(_lvlSmeltResult.registryAccess()).copy()).orElse(ItemStack.EMPTY)
+										: ItemStack.EMPTY).getItem()
+								&& (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(2)).getItem() : ItemStack.EMPTY).getCount() < 64)
 				&& (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY).getItem() == PaladiumModItems.FURNACE_UPGRADE
 						.get()) {
-			PaladiumMod.LOGGER.info("");
-		} else if (!((entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY).getItem() == PaladiumModItems.FURNACE_UPGRADE
-				.get())) {
-			PaladiumMod.LOGGER.info("");
+			fuel_power = ForgeHooks.getBurnTime((entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(0)).getItem() : ItemStack.EMPTY), null) / 200;
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				((Slot) _slots.get(0)).remove(1);
+				_player.containerMenu.broadcastChanges();
+			}
+			if (true) {
+			}
+		} else if ((entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(0)).getItem() : ItemStack.EMPTY)
+				.is(ItemTags.create(new ResourceLocation("minecraft:combustible"))) && world instanceof Level _level22 && _level22.getRecipeManager().getRecipeFor(RecipeType.SMELTING, new SimpleContainer((new Object() {
+					public ItemStack getItemStack(int sltid, ItemStack _isc) {
+						AtomicReference<ItemStack> _retval = new AtomicReference<>(ItemStack.EMPTY);
+						_isc.getCapability(ForgeCapabilities.ITEM_HANDLER, null).ifPresent(capability -> {
+							_retval.set(capability.getStackInSlot(sltid).copy());
+						});
+						return _retval.get();
+					}
+				}.getItemStack(1,
+						itemstack))), _level22).isPresent()
+				&& ((entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(2)).getItem() : ItemStack.EMPTY).getItem() == Blocks.AIR.asItem()
+						|| (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(2)).getItem() : ItemStack.EMPTY)
+								.getItem() == (world instanceof Level _lvlSmeltResult
+										? _lvlSmeltResult.getRecipeManager()
+												.getRecipeFor(RecipeType.SMELTING,
+														new SimpleContainer(
+																(entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY)),
+														_lvlSmeltResult)
+												.map(recipe -> recipe.getResultItem(_lvlSmeltResult.registryAccess()).copy()).orElse(ItemStack.EMPTY)
+										: ItemStack.EMPTY).getItem()
+								&& (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(2)).getItem() : ItemStack.EMPTY).getCount() < 64)
+				&& !((entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY).getItem() == PaladiumModItems.FURNACE_UPGRADE
+						.get())) {
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				((Slot) _slots.get(0)).remove(1);
+				_player.containerMenu.broadcastChanges();
+			}
 		}
 	}
 }
